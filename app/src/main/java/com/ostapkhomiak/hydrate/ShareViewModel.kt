0521fa3378ce96cbc881457415bef1c39ee77262
Ostap Khomiak <hamster.ostap@gmail.com>
2025-03-20 10:50:34 +0100

    add sharedPreferences to store data

--- app/src/main/java/com/ostapkhomiak/hydrate/ShareViewModel.kt
+++ app/src/main/java/com/ostapkhomiak/hydrate/ShareViewModel.kt
@@ -1,22 +1,38 @@
 package com.ostapkhomiak.hydrate
 
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 
-class ShareViewModel : ViewModel() {
 
-    val weight = MutableLiveData<Double>().apply { value = 66.0 }
-    val calculatedAmount =
-        MutableLiveData<Int>().apply { value = (weight.value?.div(22)?.toInt()?.times(1000)) }
-    val isWeightInLB = MutableLiveData<Boolean>().apply { value = false }
-    val isManualAmount = MutableLiveData<Boolean>().apply { value = false }
-    val manualAmount = MutableLiveData<Int>().apply { value = 3000 }
-    val consumedWater = MutableLiveData<Int>().apply { value = 0}
+class ShareViewModel(application: Application) : AndroidViewModel(application) {
+    private val sharedPreferences = application.getSharedPreferences("hydrate_pref", Context.MODE_PRIVATE)
+
+    val weight = MutableLiveData<Double>().apply {
+        value = sharedPreferences.getFloat("weight", 66f).toDouble()
+    }
+    val calculatedAmount = MutableLiveData<Int>().apply {
+        value = weight.value?.div(22)?.toInt()?.times(1000)?.let { sharedPreferences.getInt("calculatedAmount", it) }
+    }
+    val isWeightInLB = MutableLiveData<Boolean>().apply {
+        value = sharedPreferences.getBoolean("isWeightInLB", false)
+    }
+    val isManualAmount = MutableLiveData<Boolean>().apply {
+        value = sharedPreferences.getBoolean("isManualAmount", false)
+    }
+    val manualAmount = MutableLiveData<Int>().apply {
+        value = sharedPreferences.getInt("manualAmount", 3000)
+    }
+    val consumedWater = MutableLiveData<Int>().apply {
+        value = sharedPreferences.getInt("consumedWater", 0)
+    }
 
 
     fun setWeight(newWeight: Double) {
         weight.value = newWeight
+        sharedPreferences.edit().putFloat("weight", newWeight.toFloat()).apply()
     }
 
     fun getWeight(): Double? {
@@ -34,11 +50,13 @@ class ShareViewModel : ViewModel() {
         } else {
             calculatedAmount.value = (weight.value?.div(48)?.toInt()?.times(1000))
         }
+        sharedPreferences.edit().putInt("calculatedAmount", calculatedAmount.value!!).apply()
     }
 
 
     fun setIsWeightInLB(newWeightInLB: Boolean) {
         isWeightInLB.value = newWeightInLB
+        sharedPreferences.edit().putBoolean("isWeightInLB", newWeightInLB).apply()
     }
 
     fun getIsWeightInLB(): Boolean? {
@@ -48,6 +66,7 @@ class ShareViewModel : ViewModel() {
 
     fun setIsManualAmount(newManualAmount: Boolean) {
         isManualAmount.value = newManualAmount
+        sharedPreferences.edit().putBoolean("isManualAmount", newManualAmount).apply()
     }
 
     fun getIsManualAmount(): Boolean? {
@@ -57,6 +76,7 @@ class ShareViewModel : ViewModel() {
 
     fun setManualAmount(newAmount: Int) {
         manualAmount.value = newAmount
+        sharedPreferences.edit().putInt("manualAmount", newAmount).apply()
     }
 
     fun getManualAmount(): Int? {
@@ -66,6 +86,7 @@ class ShareViewModel : ViewModel() {
 
     fun addConsumedWater(drinkedWater: Int) {
         consumedWater.value = consumedWater.value?.plus(drinkedWater)
+        sharedPreferences.edit().putInt("consumedWater", consumedWater.value!!).apply()
     }
 
     fun getConsumedWater(): Int? {
